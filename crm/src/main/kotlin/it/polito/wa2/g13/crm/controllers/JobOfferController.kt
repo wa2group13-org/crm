7c5fb0748c55b7a32ab68181aaebc6730e80f5e4
Andrea Bonvissuto <bonvissuto32@gmail.com>
2024-05-09 17:13:15 +0200

    feat: added JobOfferController

--- crm/src/main/kotlin/it/polito/wa2/g13/crm/controllers/JobOfferController.kt
+++ crm/src/main/kotlin/it/polito/wa2/g13/crm/controllers/JobOfferController.kt
@@ -1,8 +1,93 @@
 package it.polito.wa2.g13.crm.controllers
 
-import org.springframework.web.bind.annotation.RestController
+import it.polito.wa2.g13.crm.dtos.*
+import it.polito.wa2.g13.crm.services.JobOfferService
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Min
+import org.springframework.data.domain.Page
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.*
 
 @RestController
-class JobOfferController {
+@RequestMapping("/joboffers")
+@Validated
+class JobOfferController(
+    private val jobOfferService: JobOfferService,
+) {
 
+    @GetMapping("/{jobOfferId}")
+    fun getJobOfferById(@PathVariable @Valid @Min(0) jobOfferId: Long): JobOfferDTO {
+        return jobOfferService.getJobOfferById(jobOfferId)
+    }
+
+    @GetMapping("", "/")
+    fun getJobOffers(
+        @Valid getJobOffers: GetJobOffers
+    ): Page<JobOfferDTO> {
+        println(getJobOffers)
+        return jobOfferService.getJobOffersByParams(
+            getJobOffers.filters,
+            getJobOffers.page,
+            getJobOffers.limit
+        )
+    }
+
+    @PostMapping("")
+    fun createJobOffer(@RequestBody createJobOfferDTO: CreateJobOfferDTO): JobOfferDTO {
+        return jobOfferService.createJobOffer(createJobOfferDTO)
+    }
+
+    @GetMapping("/{jobOfferId}/value")
+    fun getJobOfferValue(@PathVariable @Valid @Min(0) jobOfferId: Long): Double {
+        return jobOfferService.getJobOfferValue(jobOfferId)
+    }
+
+    @PostMapping("/{jobOfferId}")
+    fun changeJobOfferStatus(
+        @PathVariable @Valid @Min(0) jobOfferId: Long,
+        @RequestBody @Valid updateJobOfferStatusDTO: UpdateJobOfferStatusDTO
+    ): JobOfferDTO {
+        return jobOfferService.updateJobOfferStatus(jobOfferId, updateJobOfferStatusDTO)
+    }
+
+    @PutMapping("/{jobOfferId}/details")
+    fun changeJobOfferDetails(
+        @PathVariable @Valid @Min(0) jobOfferId: Long,
+        @RequestBody @Valid updateJobOfferDetailsDTO: UpdateJobOfferDetailsDTO
+    ): JobOfferDTO {
+        return jobOfferService.updateJobOfferDetails(jobOfferId, updateJobOfferDetailsDTO)
+    }
+
+
+    @DeleteMapping("/{jobOfferId}")
+    fun deleteJobOffer(@PathVariable @Valid @Min(0) jobOfferId: Long) {
+        return jobOfferService.deleteJobOffer(jobOfferId)
+    }
+
+    @GetMapping("/{jobOfferId}/notes/{noteId}")
+    fun getNoteById(@PathVariable jobOfferId: Long, @PathVariable noteId: Long): JobOfferHistoryDTO {
+        return jobOfferService.getNoteById(jobOfferId, noteId)
+    }
+
+    @GetMapping("/{jobOfferId}/notes")
+    fun getNotesByJobOfferId(@PathVariable jobOfferId: Long): List<JobOfferHistoryDTO> {
+        return jobOfferService.getNotesByJobOfferId(jobOfferId)
+    }
+
+    @PostMapping("/{jobOfferId}/notes")
+    fun addNoteByJobOfferId(
+        @PathVariable jobOfferId: Long,
+        @RequestBody addJobOfferHistoryDTO: AddJobOfferHistoryDTO
+    ): JobOfferHistoryDTO {
+        return jobOfferService.addNoteByJobOfferId(jobOfferId, addJobOfferHistoryDTO)
+    }
+
+    @PutMapping("/{jobOfferId}/notes/{noteId}")
+    fun updateNoteById(
+        @PathVariable jobOfferId: Long,
+        @PathVariable noteId: Long,
+        @RequestBody note: String?
+    ): JobOfferHistoryDTO {
+        return jobOfferService.updateNoteById(jobOfferId, noteId, note)
+    }
 }
