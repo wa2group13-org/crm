4ced1f45f618cb49e72bb7461b5cc40c55578525
Salvatore Giarracca <salvogiarracca@gmail.com>
2024-05-14 00:00:39 +0200

    fix: changed class to data class

--- crm/src/main/kotlin/it/polito/wa2/g13/crm/dtos/ContactIdDTO.kt
+++ crm/src/main/kotlin/it/polito/wa2/g13/crm/dtos/ContactIdDTO.kt
@@ -2,7 +2,7 @@ package it.polito.wa2.g13.crm.dtos
 
 import jakarta.validation.constraints.Min
 
-class ContactIdDTO (
+data class ContactIdDTO(
     @field:Min(0)
-    val contactId : Long
+    val contactId: Long
 )
--- crm/src/main/kotlin/it/polito/wa2/g13/crm/dtos/CustomerNoteDTO.kt
+++ crm/src/main/kotlin/it/polito/wa2/g13/crm/dtos/CustomerNoteDTO.kt
@@ -3,7 +3,7 @@ package it.polito.wa2.g13.crm.dtos
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Size
 
-class CustomerNoteDTO (
+data class CustomerNoteDTO(
     @Size(max = 255) @NotBlank
     val note: String?
 )
