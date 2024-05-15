39a532948d2df392f9995038b24d3d955d42c61c
Alessandro Ciullo <alessandrociullo001@gmail.com>
2024-05-15 20:19:08 +0200

    added customer controller test

--- crm/src/test/kotlin/it/polito/wa2/g13/crm/controllers/CustomerControllerTest.kt
+++ crm/src/test/kotlin/it/polito/wa2/g13/crm/controllers/CustomerControllerTest.kt
@@ -4,11 +4,11 @@ import it.polito.wa2.g13.crm.IntegrationTest
 import it.polito.wa2.g13.crm.data.contact.ContactCategory
 import it.polito.wa2.g13.crm.dtos.*
 import it.polito.wa2.g13.crm.services.ContactService
-import it.polito.wa2.g13.crm.services.ContactServiceImplTest
 import it.polito.wa2.g13.crm.services.CustomerService
 import it.polito.wa2.g13.crm.services.CustomerServiceImpl
 import it.polito.wa2.g13.crm.utils.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
@@ -19,7 +19,6 @@ import org.springframework.boot.test.web.client.exchange
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.http.RequestEntity
-import org.springframework.http.ResponseEntity
 import org.springframework.test.context.jdbc.Sql
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -28,24 +27,41 @@ class CustomerControllerTest : IntegrationTest() {
     companion object {
         private val logger = LoggerFactory.getLogger(CustomerServiceImpl::class.java)
     }
+
     @Autowired
     private lateinit var contactService: ContactService
+
     @Autowired
     private lateinit var customerService: CustomerService
+
     @Autowired
     private lateinit var restClient: TestRestTemplate
 
 
-    private val contacts = randomContacts(10, 5).map { it.copy(category = ContactCategory.Unknown) }
+    fun addRandomContactToDB() : ContactDTO{
+        val createContactDTO = randomContact().copy(category = ContactCategory.Unknown)
+        val req1 = RequestEntity.post("/API/contacts").body(createContactDTO, CreateContactDTO::class.java)
+        val res1 = restClient.exchange<ContactDTO>(req1)
+
+        assertNotNull(res1.body)
+        return res1.body!!
+    }
+
+    private val createContacts = randomContacts(10, 5).map { it.copy(category = ContactCategory.Unknown) }
     private val customers = mutableListOf<CustomerDTO>()
+    private val contacts = mutableListOf<ContactDTO>()
+
+
 
     @BeforeEach
-    fun init(){
+    fun init() {
         customers.clear()
-        contacts.forEach{ contact ->
-            val contactId = contactService.createContact(contact)
-            val customer = customerService.createCustomer(contactId)
-            customers.add(customer)
+        contacts.clear()
+        createContacts.forEach { contact ->
+            val contactDTO = contactService.createContact(contact)
+            val customerDTO = customerService.createCustomer(contactDTO.id)
+            contacts.add(contactDTO)
+            customers.add(customerDTO)
         }
         logger.info("Initialized DB")
 
@@ -68,27 +84,142 @@ class CustomerControllerTest : IntegrationTest() {
         val req = RequestEntity.get("/API/customers/$id").build()
         val res = restClient.exchange<ProblemDetail>(req)
 
-        assertEquals( HttpStatus.NOT_FOUND, res.statusCode)
+        assertEquals(HttpStatus.NOT_FOUND, res.statusCode)
     }
 
     @Test
-    fun `get customer by valid id`(){
+    fun `get customer by valid id`() {
         val id = customers[0].id
         val req = RequestEntity.get("/API/customers/$id").build()
         val res = restClient.exchange<CustomerDTO>(req)
 
-        assertEquals( HttpStatus.OK, res.statusCode)
+        assertEquals(HttpStatus.OK, res.statusCode)
         assertRecursive(customers[0], res.body)
     }
 
     @Test
-    fun `create new customer`(){
-        val createContact = randomContact()
-        val req1 = RequestEntity.post("/API/contacts").body(createContact, CreateContactDTO::class.java)
-        val res1 = restClient.exchange<RequestEntity<*>>(req1)
+    fun `create new valid customer`() {
+        val createContactDTO = randomContact().copy(category = ContactCategory.Unknown)
+        val req1 = RequestEntity.post("/API/contacts").body(createContactDTO, CreateContactDTO::class.java)
+        val res1 = restClient.exchange<ContactDTO>(req1)
 
-        //val req2 = RequestEntity.get(res1.headers.location).build()
-        //val contact = restClient.exchange<ContactDTO>(req2)
+        assertNotNull(res1.body)
+
+        val contactDTO = res1.body!!
+        val req2 = RequestEntity.post("/API/customers").body(ContactIdDTO(contactDTO.id), ContactIdDTO::class.java)
+        val res2 = restClient.exchange<CustomerDTO>(req2)
+
+        assertNotNull(res2.body)
+        val customerDTO = res2.body!!
+
+        assertEquals(contactDTO.copy(category = ContactCategory.Customer), customerDTO.contact)
+    }
+
+    @Test
+    fun `failing to create customer because contact id not valid 404`() {
+        val req = RequestEntity.post("/API/customers").body(ContactIdDTO(-1), ContactIdDTO::class.java)
+        val res = restClient.exchange<ProblemDetail>(req)
+
+        assertEquals(HttpStatus.NOT_FOUND, res.statusCode)
+    }
+
+    @Test
+    fun `failing to create a customer, contact already linked to someone else`() {
+        val createContactDTO = randomContact().copy(category = ContactCategory.Customer)
+        val req1 = RequestEntity.post("/API/contacts").body(createContactDTO, CreateContactDTO::class.java)
+        val res1 = restClient.exchange<ContactDTO>(req1)
+
+        assertNotNull(res1.body)
+
+        val contactDTO = res1.body!!
+        val req2 = RequestEntity.post("/API/customers").body(ContactIdDTO(contactDTO.id), ContactIdDTO::class.java)
+        val res2 = restClient.exchange<ProblemDetail>(req2)
+
+        assertEquals(HttpStatus.FORBIDDEN, res2.statusCode)
+    }
+
+    @Test
+    fun `delete customer, expected 204`() {
+        val customerId = customers[0].id
+        val req1 = RequestEntity.delete("/API/customers/$customerId").build()
+        val res1 = restClient.exchange<Unit>(req1)
+
+        assertEquals(HttpStatus.NO_CONTENT, res1.statusCode)
+
+        val req2 = RequestEntity.get("/API/customers/$customerId").build()
+        val res2 = restClient.exchange<ProblemDetail>(req2)
+
+        assertEquals(HttpStatus.NOT_FOUND, res2.statusCode)
+    }
+
+    @Test
+    fun `delete non existent customer, expected 404`() {
+        val req = RequestEntity.get("/API/customers/-1").build()
+        val res = restClient.exchange<ProblemDetail>(req)
+
+        assertEquals(HttpStatus.NOT_FOUND, res.statusCode)
+    }
+
+    @Test
+    fun `update customer note, expected 200`() {
+        val customerId = customers[0].id
+        val note = "My Test Note"
+        val req = RequestEntity.put("/API/customers/$customerId/note").body(CustomerNoteDTO("My Test Note"))
+        val res = restClient.exchange<Unit>(req)
+
+        assertEquals(HttpStatus.OK, res.statusCode)
+
+        val req2 = RequestEntity.get("/API/customers/$customerId").build()
+        val res2 = restClient.exchange<CustomerDTO>(req2)
+
+        val customerDTO = res2.body!!
+
+        assertEquals(note, customerDTO.note)
+    }
+
+    @Test
+    fun `update absent customer note, expected 404 `() {
+        val customerId = -1
+        val note = "My Test Note"
+        val req = RequestEntity.put("/API/customers/$customerId/note").body(CustomerNoteDTO("My Test Note"))
+        val res = restClient.exchange<ProblemDetail>(req)
+
+        assertEquals(HttpStatus.NOT_FOUND, res.statusCode)
+    }
+
+    @Test
+    fun `update customer contact, expected 200`() {
+        val contactDTO = addRandomContactToDB()
+        val customerId = customers[0].id
+        val req = RequestEntity.put("/API/customers/$customerId/contact").body(ContactIdDTO(contactDTO.id))
+        val res = restClient.exchange<Unit>(req)
+
+        assertEquals(HttpStatus.OK, res.statusCode)
+
+        val req2 = RequestEntity.get("/API/customers/$customerId").build()
+        val res2 = restClient.exchange<CustomerDTO>(req2)
+
+        val customerDTO = res2.body!!
+
+        assertEquals(contactDTO, customerDTO.contact)
+    }
+
+    @Test
+    fun `update customer contact with an absent contact, expected 404` () {
+        val customerId = customers[0].id
+        val req = RequestEntity.put("/API/customers/$customerId/contact").body(ContactIdDTO(-1))
+        val res = restClient.exchange<ProblemDetail>(req)
+
+        assertEquals(HttpStatus.NOT_FOUND, res.statusCode)
+    }
+
+    @Test
+    fun `update customer contact with an illegal contact, expected 403` () {
+        val customerId = customers[0].id
+        val req = RequestEntity.put("/API/customers/$customerId/contact").body(ContactIdDTO(customers[1].contact.id))
+        val res = restClient.exchange<ProblemDetail>(req)
+
+        assertEquals(HttpStatus.FORBIDDEN, res.statusCode)
     }
 
 }
